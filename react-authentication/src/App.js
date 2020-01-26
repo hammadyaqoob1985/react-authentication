1bfb98d220fa627c365f05737750bfdff6862594
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2020-01-26 16:17:53 +0000

    added ability to login and call back

--- react-authentication/src/App.js
+++ react-authentication/src/App.js
@@ -3,17 +3,28 @@ import { Route } from "react-router-dom";
 import Home from "./Home";
 import Profile from "./Profile";
 import Nav from "./Nav";
+import Auth from "./Auth/Auth";
 
-function App() {
-  return (
-    <>
-      <Nav />
-      <div className="body">
-        <Route path="/" exact component={Home} />
-        <Route path="/profile" component={Profile} />
-      </div>
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
+  render() {
+    return (
+      <>
+        <Nav />
+        <div className="body">
+          <Route
+            path="/"
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route path="/profile" component={Profile} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
